629a1720fb984a2c41b13c4f968bf9a1266fdd65
Johnny Klucinec <jsklucinec@gmail.com>
2024-07-30 21:17:01 -0400

    I can now see the dongle in rust :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rusb::{Context, Error, UsbContext};
+
+fn main() -> Result<(), Error> {
+    let context = Context::new()?;
+    const VID: u16 = 0x361d; // Finalmouse
+    const PID: u16 = 0x0100; // UltralightX dongle
+
+    let device = context.devices()?.iter().find(|device| {
+        let device_desc = device.device_descriptor().unwrap();
+        device_desc.vendor_id() == VID && device_desc.product_id() == PID
+    });
+
+    println!("Device found: {:?}", device);
+
+    Ok(())
 }
