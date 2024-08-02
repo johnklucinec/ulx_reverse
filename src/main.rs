827f6d588911ce418dae56a4253ddad1c706ceee
Johnny Klucinec <jsklucinec@gmail.com>
2024-08-01 21:02:23 -0400

    im lost

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
-use rusb::{Context, Error, UsbContext};
+use rusb::{Context, Device, DeviceHandle, Error, UsbContext};
+use tokio;
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let context = Context::new()?;
-    const VID: u16 = 0x361d; // Finalmouse
-    const PID: u16 = 0x0100; // UltralightX dongle
+    //const VID: u16 = 0x361d; // Finalmouse
+    //const PID: u16 = 0x0100; // UltralightX dongle
 
+    const VID: u16 = 0x3151; // Monsgeek
+    const PID: u16 = 0x4015; // Keyboard?
+
+    // Find the device
     let device = context.devices()?.iter().find(|device| {
         let device_desc = device.device_descriptor().unwrap();
         device_desc.vendor_id() == VID && device_desc.product_id() == PID
     });
 
-    println!("Device found: {:?}", device);
+    match device {
+        Some(device) => {
+            println!("Device found: {:?}", device);
+
+            let mut handle = match device.open() {
+                Ok(handle) => {
+                    println!("Device opened");
+                    handle
+                }
+                Err(e) => return Err(Box::new(e) as Box<dyn std::error::Error + 'static>),
+            };
+
+            // Claim the first interface
+            let interface_number = 0;
+            if let Err(e) = handle.claim_interface(interface_number) {
+                eprintln!("Failed to claim interface {}: {}", interface_number, e);
+                return Err(Box::new(e) as Box<dyn std::error::Error + 'static>);
+            }
+
+            // Release the interface
+            if let Err(e) = handle.release_interface(interface_number) {
+                eprintln!("Failed to release interface {}: {}", interface_number, e);
+                return Err(Box::new(e) as Box<dyn std::error::Error + 'static>);
+            }
+        }
+        None => {
+            println!("Device not found!");
+        }
+    }
 
     Ok(())
 }
